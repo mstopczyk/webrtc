fcf11a74a61a17039a7f656fa8ac1a2a23af3797
Maciej Stopczyk <maciek.stopczyk@gmail.com>
2014-05-27 08:08:07 +0200

    update joinout fe to use node

---
+++ joinout_fe/server.js
@@ -0,0 +1,19 @@
+// set up ======================================================================
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var port  	 = 9090; 				// set the port
+
+// configuration ===============================================================
+app.configure(function() {
+	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.logger('dev')); 						// log every request to the console
+	app.use(express.bodyParser()); 							// pull information from html in POST
+	app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
